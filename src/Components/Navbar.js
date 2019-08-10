88dd67454ebda5d34595c23632eb470951f8a01b
Patrick Heflin <patrickslx@yahoo.com>
2019-08-09 23:01:45 -0500

    navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../1stchoice.png'
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+      <div className='img-container'>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className='nav-nav'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
